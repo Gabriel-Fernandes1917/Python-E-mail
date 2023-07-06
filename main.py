9d1f1bd8e929c77e2c5cbab357b481f636d96db8
Gabriel Fernandes <gabgui2001@gmail.com>
2023-07-05 21:22:17 -0300

    .

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import mimetypes
 
 email_senha = open('pass', 'r').read()
 
-email_origem = ''
-email_destino = ('')
+email_origem = 'gabgui2001@outlook.com'
+email_destino = ('gabgui2001@gmail.com')
 
 assunto = "Ola so um test"
 
@@ -30,6 +30,7 @@ safe = ssl.create_default_context()
 
 #with open and close one connection, the pythom
 #with outher service
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=safe) as smtp:
-    smtp.login(email_origem, email_senha)
+with smtplib.SMTP('smtp.office365.com', port=587) as smtp:
+    smtp.starttls(context=safe)
+    smtp.login(email_origem, "akjonfoymnnhniny")
     smtp.sendmail(email_origem,email_destino, menssagem.as_string())
